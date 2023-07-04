c827b02678eaddaeadce62e178f6adff1a75d73a
Revenant <dasdev.pratik@gmail.com>
2023-07-04 15:46:45 +0530

    Nav layout complete 1440

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import logo from '../../public/logo.png'
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex mx-auto max-w-7xl bg-red justify-between">
+      <div><Image src={logo} alt="MUN LOGO"/></div>
+      <ul className="flex gap-28 mr-20"><li>About</li>
+        <li>Event</li>
+        <li>Speaker</li>
+        <li><button className="rounded-full border-none bg-buttonBackground px-4 py-1 outline-none">
+          REGISTER NOW
+        </button></li></ul>
+    </div>
   )
 }
 
