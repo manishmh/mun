d7fba1e1b4fa15339834c3411d28f13c2c49ceb5
Revenant <dasdev.pratik@gmail.com>
2023-07-12 14:22:07 +0530

    implemented 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,17 +3,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../public/logo.png'
 
-export default function Four0Four() {
+export default function NotFound() {
 	return (
 	    <div className="bg-[url('/Error/green_bg.png')] w-full h-full bg-no-repeat bg-cover">
-      <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center relative justify-center min-h-screen">
       <Link href='/'><Image src={logo} alt="MUN LOGO" className={  'w-[60px] h-[60px] lg:h-[90px] lg:w-[90px]'} /></Link>
-              <Image src="/Error/plugs_1.png" alt="Image" className="mt-8" />
-
-              <p>We&apos;re sorry, the page you requested could not be found Please go back to the homepage</p>
-        
+            <h2 className="text-3xl text-center absolute">Page Not Found</h2>
+              <img src="/Error/plugs_1.png" alt="Image" className="mt-8" />
+              <p className='w-84 pt-12 text-center mx-4'>We&apos;re sorry, the page you requested could not be found Please go back to the homepage</p>  
         <Link href='/'><button className="rounded-full border-none bg-buttonBackground mt-12 px-4 py-1 outline-none">
-          REGISTER NOW
+          RETURN TO HOMEPAGE
         </button></Link>
       </div>
     </div>
