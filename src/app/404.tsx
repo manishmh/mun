24e5284162a2865fc724fb3ca649488b7788c125
Revenant <dasdev.pratik@gmail.com>
2023-07-12 13:55:29 +0530

    404

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from '../../../public/logo.png'
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../../public/logo.png'
 
-
-import Link from 'next/link';
-
-const commingsoon = () => {
-  return (
-    <div className="bg-[url('/Error/green_bg.png')] w-full h-full bg-no-repeat bg-cover">
+export default function Four0Four() {
+	return (
+	    <div className="bg-[url('/Error/green_bg.png')] w-full h-full bg-no-repeat bg-cover">
       <div className="flex flex-col items-center justify-center min-h-screen">
       <Link href='/'><Image src={logo} alt="MUN LOGO" className={  'w-[60px] h-[60px] lg:h-[90px] lg:w-[90px]'} /></Link>
               <Image src="/Error/plugs_1.png" alt="Image" className="mt-8" />
 
-              <p></p>
+              <p>We&apos;re sorry, the page you requested could not be found Please go back to the homepage</p>
         
         <Link href='/'><button className="rounded-full border-none bg-buttonBackground mt-12 px-4 py-1 outline-none">
-          RETURN TO HOMEPAGE
+          REGISTER NOW
         </button></Link>
       </div>
     </div>
-  );
-};
-
-export default commingsoon;
+	)
+  }
+  
