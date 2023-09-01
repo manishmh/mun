3cb993d1a520f54bcf9f2b098b9d75c29c4e7b09
Sohini Joarder <sohini.joarder@gmail.com>
2023-09-01 10:57:27 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -88,6 +88,8 @@ const Hero = () => {
             <h1 className="text-[43px] md:text-3xl font-bold text-center">WHO WE ARE:</h1>
             <p className="text-sm leading-5 mt-4 break-words whitespace-pre-line text-justify">
             KIIT International Model United Nations emerges as a leading global United Nations simulation, spearheaded by the Secretariat, the committed student body of the Kalinga Institute of Industrial Technology. Since its inception in 2013, this conference has consistently surpassed expectations, establishing unprecedented benchmarks year after year. By fostering intellectual growth, nurturing youth leadership, and promoting global understanding, it stands as an exemplary platform for empowerment and enlightenment.
+            <br /><br />
+              In case there is a discrepancy regarding the allotted committee, please fill out this form to apply for a CHANGE of COMMITTEE
             </p>
             <div className="md:hidden flex gap-2 mt-2 text-lg font-bold justify-center md:justify-normal">
               See more about us
@@ -98,9 +100,9 @@ const Hero = () => {
               </Link>
             </div>
             <div className="flex justify-center md:justify-center">
-              <Link href="mun" className="z-30" target="blank">
+              <Link href="https://forms.gle/vRZPrNXAaa4Yq7GbA" className="z-30" target="blank">
                 <button className="mt-12  md:mt-5 rounded-full border-none bg-buttonBackground px-10 py-1.5 text-base outline-none text-xl">
-                  EARLY BIRD REGISTRATION END !!
+                  Form for Committee Change
                 </button>
               </Link>
             </div>
